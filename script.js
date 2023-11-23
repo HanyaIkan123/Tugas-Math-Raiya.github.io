2ee343eceb8e92e397645e476c8d1f782fc76e1b
HanyaIkan123 <148044559+HanyaIkan123@users.noreply.github.com>
2023-11-23 20:50:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+// Contoh Math.round
+let angkaARound = 7.49;
+let angkaBRound = 10.51;
+let angkaCRound = 3.75;
+
+let hasilRound1 = Math.round(angkaARound);
+let hasilRound2 = Math.round(angkaBRound);
+let hasilRound3 = Math.round(angkaCRound);
+
+console.log("Hasil Math.round 1:", hasilRound1);
+console.log("Hasil Math.round 2:", hasilRound2);
+console.log("Hasil Math.round 3:", hasilRound3);
+
+// Contoh Math.floor
+let angkaAFloor = 5.99;
+let angkaBFloor = 6.75;
+let angkaCFloor = 4.5;
+
+let hasilFloor1 = Math.floor(angkaAFloor);
+let hasilFloor2 = Math.floor(angkaBFloor);
+let hasilFloor3 = Math.floor(angkaCFloor);
+
+console.log("Hasil Math.floor 1:", hasilFloor1);
+console.log("Hasil Math.floor 2:", hasilFloor2);
+console.log("Hasil Math.floor 3:", hasilFloor3);
+
+// Contoh Math.ceil
+let angkaACeil = 2.01;
+let angkaBCeil = 9.5;
+let angkaCCeil = 6.4;
+
+let hasilCeil1 = Math.ceil(angkaACeil);
+let hasilCeil2 = Math.ceil(angkaBCeil);
+let hasilCeil3 = Math.ceil(angkaCCeil);
+
+console.log("Hasil Math.ceil 1:", hasilCeil1);
+console.log("Hasil Math.ceil 2:", hasilCeil2);
+console.log("Hasil Math.ceil 3:", hasilCeil3);
+
+// Contoh Math.pow
+let angkaAPow = 2;
+let angkaBPow = 3;
+let angkaCPow = 4;
+
+let hasilPow1 = Math.pow(angkaAPow, 3);
+let hasilPow2 = Math.pow(angkaBPow, 2);
+let hasilPow3 = Math.pow(angkaCPow, 2);
+
+console.log("Hasil Math.pow 1:", hasilPow1);
+console.log("Hasil Math.pow 2:", hasilPow2);
+console.log("Hasil Math.pow 3:", hasilPow3);
+
+// Contoh Math.sqrt
+let angkaASqrt = 25;
+let angkaBSqrt = 16;
+let angkaCSqrt = 9;
+
+let hasilSqrt1 = Math.sqrt(angkaASqrt);
+let hasilSqrt2 = Math.sqrt(angkaBSqrt);
+let hasilSqrt3 = Math.sqrt(angkaCSqrt);
+
+console.log("Hasil Math.sqrt 1:", hasilSqrt1);
+console.log("Hasil Math.sqrt 2:", hasilSqrt2);
+console.log("Hasil Math.sqrt 3:", hasilSqrt3);
